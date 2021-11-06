69b70d883b7d4e3c7eae979bb700ac792da31b07
MounaKossentini <60269332+MounaKossentini@users.noreply.github.com>
2021-11-06 18:33:17 +0100

    upload

--- app/src/main/java/com/example/ala_dhawki/SignupActivity.java
+++ app/src/main/java/com/example/ala_dhawki/SignupActivity.java
@@ -1,23 +1,52 @@
 package com.example.ala_dhawki;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SignupActivity extends AppCompatActivity {
 
     private Button register;
     private Button signup;
     private Button signin;
+    private ImageView upload;
+    private FloatingActionButton uploadbtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
 
         register = (Button) findViewById(R.id.button3);
+        upload = (ImageView) findViewById(R.id.upload);
+        uploadbtn = (FloatingActionButton) findViewById(R.id.uploadbtn);
+
+        uploadbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.Companion.with(SignupActivity.this)
+                        .crop()
+                        .cropSquare()
+                        .compress(1024)
+                        .maxResultSize(511, 511)
+                        .start();
+            }
+        });
+
+
+
+
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,5 +76,12 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        upload.setImageURI(uri);
     }
 }
